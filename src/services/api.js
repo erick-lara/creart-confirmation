b53beede4a9c778f8ce291aafe90c1cb09dab39e
erick.penetta <erick.penetta@valemobi.com.br>
2022-04-22 02:20:00 -0300

    teste https

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://creart-scheduler.herokuapp.com/api/scheduler-ws/mails/confirm",
+    httpsAgent: new https.Agent({  
+        rejectUnauthorized: false
+    })
 });
 
 export default api;
