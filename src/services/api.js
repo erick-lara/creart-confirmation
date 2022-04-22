944a200b7aa008d8ac2a4b0028a1a3500be041ad
erick.penetta <erick.penetta@valemobi.com.br>
2022-04-21 21:41:55 -0300

    refactor: url correta

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/scheduler-ws/mails/",
+    baseURL: "http://localhost:8080/api/scheduler-ws/mails/confirm?token=",
 });
 
 export default api;
