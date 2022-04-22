1e330f5cdae8d45dc7b911051ad0f2aee3ac959f
erick.penetta <erick.penetta@valemobi.com.br>
2022-04-22 02:51:55 -0300

    fix: procces.env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const api = axios.create({
     baseURL: "https://creart-scheduler.herokuapp.com/api/scheduler-ws/mails/confirm",
