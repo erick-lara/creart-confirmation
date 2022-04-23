5b8a3b3a062089cf2732758e9a8ccbeb62b347fc
Erick Lara <egmlara81@gmail.com>
2022-04-23 03:48:51 -0300

    refactor: corrigindo URL de chamada da API para azure

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://creart-scheduler.herokuapp.com/api/scheduler-ws/mails/confirm",
+    baseURL: "https://creart-scheduler.azurewebsites.net/api/scheduler-ws/mails/confirm",
 });
 
 export default api;
