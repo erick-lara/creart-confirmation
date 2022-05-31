1a645e614c0e832a0a4d2b669714d4b0bd08ee48
erick.penetta <erick.penetta@valemobi.com.br>
2022-05-31 15:08:05 -0300

    fix: ajuste url aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/social-ws/admin/confirm",
+    baseURL: "http://ec2-54-233-130-250.sa-east-1.compute.amazonaws.com:8080/api/social-ws/admin/confirm",
 });
 
 export default api;
